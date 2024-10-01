8932e94641b73197b5e49c8cf504c5c87156e10a
Mike Miller <mikej.miller440@gmail.com>
2024-10-01 16:36:27 -0500

    about page changes

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,20 +1,37 @@
 import { ImageHeadingText } from "~/components";
+import { Card, CardContent } from "~/components/ui/card";
 import urbanImage from "~/img/urban-outside.jpg";
 
 export default function about() {
   return (
-    <section>
-      <div className="relative mt-5 h-[50vh] w-full">
+    <section className="flex flex-col items-center">
+      <div className="relative mt-5 flex h-[50vh] w-full">
         <img
-          className="size-full object-cover"
+          className="size-full object-cover brightness-50"
           src={urbanImage}
           alt="An outside view of Urban Olive and Vine"
         />
         <ImageHeadingText className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
-          Nurture Your Senses
+          Our Story
         </ImageHeadingText>
       </div>
-      <h1>About</h1>
+      <h1 className="p-5 text-center text-5xl">Nurture Your Senses</h1>
+      <Card className="w-4/5">
+        <CardContent>
+          <p className="p-5">
+            More than just a bistro, we created Urban Olive & Vine to be a place
+            where you can nurture your senses and creativity through great food,
+            fine wine, live music, local art and merchandise, and events like
+            painting classes with Audrey Martin. Listen to live music, share
+            some small plates with friends, try a local craft beer, or find a
+            quiet corner to relax with a good book over a cup of coffee or tea.
+            Weather permitting, bring your favorite four-legged pals to our
+            cozy, dog-friendly patio and pamper them with a treat from a menu
+            made just for them. Owned and operated by longtime Hudson residents
+            Carol and Chad, we invite you to indulge your senses. Enjoy!
+          </p>
+        </CardContent>
+      </Card>
     </section>
   );
 }
