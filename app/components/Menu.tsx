87afdf0ace0471ab482a8b8e75cbe7e99186761a
Mike Miller <mikej.miller440@gmail.com>
2024-10-03 12:39:42 -0500

    menu edits

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,19 +1,24 @@
 import { cn } from "~/lib/utils";
+import { GlutenFreeIcon, VeganIcon } from "./svg";
+
+type MenuItem = {
+  name: string;
+  description: string;
+  price: string;
+  isVegan?: boolean;
+  isGlutenFree?: boolean;
+  isSpecial?: boolean;
+};
+
+type MenuSelection = {
+  title: string;
+  notes: string[];
+  items: MenuItem[];
+};
 
 type MenuSection = {
   menuTitle: string;
-  menuSelections: {
-    title: string;
-    notes: string[];
-    items: {
-      name: string;
-      description: string;
-      price: string;
-      isVegan?: boolean;
-      isGlutenFree?: boolean;
-      isSpecial?: boolean;
-    }[];
-  }[];
+  menuSelections: MenuSelection[];
 };
 
 type MenusProps = React.HTMLAttributes<HTMLDivElement> & {
@@ -24,15 +29,85 @@ export default function Menus({ menus, className, ...props }: MenusProps) {
   return (
     <section className={cn("", className)} {...props}>
       {menus.map((currentMenu, index) => {
-        return <div></div>;
+        return <MenuSection key={index} menu={currentMenu} />;
       })}
     </section>
   );
 }
 
-// export function MenuSection({menu: MenuSection}) {
-//   const {menuTitle, } = menu
-//   return (
-//     <h1>m</h1>
-//   )
-// }
+export function MenuSection({
+  menu,
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement> & { menu: MenuSection }) {
+  const { menuTitle, menuSelections } = menu;
+  return (
+    <div className={className} {...props}>
+      <h1>{menuTitle}</h1>
+      {menuSelections.map((currentMenuSelection, index) => {
+        return (
+          <MenuSelection menuSelection={currentMenuSelection} key={index} />
+        );
+      })}
+    </div>
+  );
+}
+
+export function MenuSelection({
+  menuSelection,
+}: React.HTMLAttributes<HTMLDivElement> & {
+  menuSelection: MenuSelection;
+}) {
+  const { title, notes, items } = menuSelection;
+  return (
+    <div>
+      <h2>{title}</h2>
+      <ul>
+        {notes.map((currentNote, index) => {
+          return (
+            <li key={index}>
+              <p>{currentNote}</p>
+            </li>
+          );
+        })}
+      </ul>
+      {items.map((currentItem, index) => {
+        return <MenuItem item={currentItem} key={index} />;
+      })}
+    </div>
+  );
+}
+
+export function MenuItem({
+  item,
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement> & {
+  item: MenuItem;
+}) {
+  const { name, description, price, isGlutenFree, isVegan, isSpecial } = item;
+  const special: React.HtmlHTMLAttributes<HTMLDivElement>["className"] =
+    // TODO Add border
+    isSpecial ? "FANCYBORDER" : "";
+  return (
+    <div className={cn(special, className)} {...props}>
+      <div>
+        <h2>
+          {isGlutenFree && (
+            <span>
+              <GlutenFreeIcon />
+            </span>
+          )}
+          {isVegan && (
+            <span>
+              <VeganIcon />
+            </span>
+          )}
+          {name}
+        </h2>
+        <p>{price}</p>
+      </div>
+      <p>{description}</p>
+    </div>
+  );
+}
