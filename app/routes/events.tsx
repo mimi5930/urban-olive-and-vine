403452a41f707e90cbda28deca0eef5b4800dacd
Mike Miller <mikej.miller440@gmail.com>
2024-10-04 23:09:20 -0500

    event page edits

--- app/routes/events.tsx
+++ app/routes/events.tsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react";
-import { EventCard } from "~/components/Events";
 import { Button, buttonVariants } from "~/components/ui/button";
 import { Calendar } from "~/components/ui/calendar";
 import { Card } from "~/components/ui/card";
 import { cn } from "~/lib/utils";
 import { mockEvents } from "../mockData";
 import { ChevronLeftIcon, ChevronRightIcon } from "~/components/svg";
+import picture from "../img/musician.jpg";
+import { type DayContentProps } from "react-day-picker";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionContent,
+  AccordionTrigger,
+} from "~/components/ui/accordion";
+import { Link } from "@remix-run/react";
+import { cva, type VariantProps } from "class-variance-authority";
 
 export default function events() {
   const [date, setDate] = useState<Date | undefined>(new Date());
@@ -13,29 +22,90 @@ export default function events() {
   // TODO: Remove this test
   const { title, date: eventDate, image, alt, description } = mockEvents[0];
 
+  const electricBananas = mockEvents.filter((event) => {
+    if (event.title.includes("Electric Bananas")) {
+      return true;
+    }
+  });
+
+  const electricBananasDates: Date[] = [];
+  electricBananas.forEach((event) => {
+    electricBananasDates.push(new Date(event.date));
+  });
+
+  console.log(electricBananasDates);
+
   return (
     <section className="mt-32 flex w-full flex-col">
-      <h1 className="text-center text-5xl">Events</h1>
+      <h1 className="pb-5 text-center text-5xl">Events</h1>
+      <h2 className="text-center text-5xl">
+        {date?.toLocaleString(undefined, {
+          weekday: "long",
+          month: "short",
+          day: "numeric",
+        })}
+      </h2>
       <div className="flex justify-center gap-5 p-5">
-        <div className="aspect-video flex-1">
+        <div className="aspect-video max-h-[30vh] w-5/12">
           <img
             className="size-full rounded-xl object-cover shadow-xl"
             src={image}
             alt={alt}
           />
         </div>
-        <div className="flex-1">
+        <div className="w-5/12">
           <h2 className="text-2xl font-semibold">{title}</h2>
           <h2 className="text-1xl">{eventDate}</h2>
           <p>{description}</p>
+          {/* <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
+            libero repellat aspernatur, reiciendis accusantium itaque cumque
+            earum, sequi delectus rem non, ullam numquam impedit molestias
+            quisquam minima velit fuga aliquid culpa nemo officiis vitae
+            pariatur? Sequi, eveniet eum! Assumenda, deleniti!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
+            libero repellat aspernatur, reiciendis accusantium itaque cumque
+            earum, sequi delectus rem non, ullam numquam impedit molestias
+            quisquam minima velit fuga aliquid culpa nemo officiis vitae
+            pariatur? Sequi, eveniet eum! Assumenda, deleniti!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
+            libero repellat aspernatur, reiciendis accusantium itaque cumque
+            earum, sequi delectus rem non, ullam numquam impedit molestias
+            quisquam minima velit fuga aliquid culpa nemo officiis vitae
+            pariatur? Sequi, eveniet eum! Assumenda, deleniti!
+          </p> */}
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Hear {title} Another Night</AccordionTrigger>
+              <AccordionContent>
+                <Link className="hover:underline" to="/events">
+                  {eventDate}
+                </Link>
+              </AccordionContent>
+              <AccordionContent>
+                <Link className="hover:underline" to="/events">
+                  {eventDate}
+                </Link>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
       </div>
       <div className="flex w-full justify-center">
         <Card>
           <Calendar
             mode="single"
+            required
             selected={date}
             onSelect={setDate}
+            // !MODIFIERS!
+            modifiers={{ electric: electricBananasDates }}
+            // modifiersClassNames={{ electric: "bg-red" }}
+            // !
             className="flex h-full w-full p-5"
             classNames={{
               months:
@@ -62,10 +132,43 @@ export default function events() {
             components={{
               IconLeft: () => <ChevronLeftIcon className="size-8" />,
               IconRight: () => <ChevronRightIcon className="size-8" />,
+              DayContent: CustomDayContent,
             }}
           />
         </Card>
       </div>
     </section>
+  );
+}
+
+export function CustomDayContent(props: DayContentProps) {
+  const dayDisplayVariants = cva(cn(buttonVariants({ variant: "ghost" }), ""), {
+    variants: {
+      variant: {
+        default:
+          "size-24 p-0 font-normal hover:bg-feldgrau hover:text-white aria-selected:opacity-100",
+        electric:
+          "size-24 p-0 font-normal hover:bg-red hover:text-white aria-selected:opacity-100",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  });
+
+  console.log(props.activeModifiers);
+
+  const hasElectric = () => {
+    if ("electric" in props.activeModifiers) {
+      return "electric";
+    } else {
+      return "default";
+    }
+  };
+
+  return (
+    <span className={dayDisplayVariants({ variant: hasElectric() })}>
+      {props.date.getDate()}
+    </span>
   );
 }
