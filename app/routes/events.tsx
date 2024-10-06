0703ea56f08791416ec412c7ce6c10eaa87ffeeb
Mike Miller <mikej.miller440@gmail.com>
2024-10-06 17:35:41 -0500

    updates to event page

--- app/routes/events.tsx
+++ app/routes/events.tsx
@@ -4,8 +4,11 @@ import { Calendar } from "~/components/ui/calendar";
 import { Card } from "~/components/ui/card";
 import { cn } from "~/lib/utils";
 import { mockEvents } from "../mockData";
-import { ChevronLeftIcon, ChevronRightIcon } from "~/components/svg";
-import picture from "~/img/musician.jpg";
+import {
+  CalendarIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "~/components/svg";
 import {
   Accordion,
   AccordionItem,
@@ -13,102 +16,126 @@ import {
   AccordionTrigger,
 } from "~/components/ui/accordion";
 import { Link } from "@remix-run/react";
+import { Event } from "~/components/Events";
 
 export default function events() {
+  function sameDay(d1: Date, d2: Date) {
+    return (
+      d1.getFullYear() === d2.getFullYear() &&
+      d1.getMonth() === d2.getMonth() &&
+      d1.getDate() === d2.getDate()
+    );
+  }
+
   const [date, setDate] = useState<Date | undefined>(new Date());
+  const [currentEvent, setCurrentEvent] = useState<Event | undefined>(
+    mockEvents.find((event) => {
+      return sameDay(new Date(event.date), new Date());
+    }),
+  );
 
-  // TODO: Remove this test
-  const { title, date: eventDate, image, alt, description } = mockEvents[0];
+  function groupObjectsByTitle(objectsArray: Event[]): {
+    [key: string]: Date[];
+  } {
+    const groupedObjects: { [key: string]: Date[] } = {};
 
-  const electricBananas = mockEvents.filter((event) => {
-    if (event.title.includes("Electric Bananas")) {
-      return true;
-    }
-  });
+    objectsArray.forEach((obj) => {
+      const dateObject = new Date(obj.date);
+      if (!groupedObjects[obj.title]) {
+        groupedObjects[obj.title] = [];
+      }
+      groupedObjects[obj.title].push(dateObject);
+    });
 
-  const electricBananasDates: Date[] = [];
-  electricBananas.forEach((event) => {
-    electricBananasDates.push(new Date(event.date));
-  });
+    return groupedObjects;
+  }
 
-  console.log(electricBananasDates);
+  const sortedEvents = groupObjectsByTitle(mockEvents);
 
   return (
     <section className="mt-32 flex w-full flex-col">
       <h1 className="pb-5 text-center text-5xl">Events</h1>
-      <h2 className="text-center text-5xl">
-        {date?.toLocaleString(undefined, {
-          weekday: "long",
-          month: "short",
-          day: "numeric",
-        })}
-      </h2>
-      <div className="flex justify-center gap-5 p-5">
-        <div className="aspect-video max-h-[30vh] w-5/12">
-          <img
-            className="size-full rounded-xl object-cover shadow-xl"
-            src={image}
-            alt={alt}
-          />
-        </div>
-        <div className="w-5/12">
-          <h2 className="text-2xl font-semibold">{title}</h2>
-          <h2 className="text-1xl">{eventDate}</h2>
-          <p>{description}</p>
-          {/* <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
-            libero repellat aspernatur, reiciendis accusantium itaque cumque
-            earum, sequi delectus rem non, ullam numquam impedit molestias
-            quisquam minima velit fuga aliquid culpa nemo officiis vitae
-            pariatur? Sequi, eveniet eum! Assumenda, deleniti!
+
+      {!currentEvent ? (
+        <div className="h-[40vh]">
+          <h2 className="text-center text-2xl">
+            {date?.toLocaleString(undefined, {
+              weekday: "long",
+              month: "short",
+              day: "numeric",
+            })}
+          </h2>
+          <h2 className="text-center text-2xl">No Live Musicians Tonight</h2>
+          <p className="text-center">
+            Check out our Event Calendar below or{" "}
+            <Link to="#" className="underline">
+              go to our next event
+            </Link>
           </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
-            libero repellat aspernatur, reiciendis accusantium itaque cumque
-            earum, sequi delectus rem non, ullam numquam impedit molestias
-            quisquam minima velit fuga aliquid culpa nemo officiis vitae
-            pariatur? Sequi, eveniet eum! Assumenda, deleniti!
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
-            libero repellat aspernatur, reiciendis accusantium itaque cumque
-            earum, sequi delectus rem non, ullam numquam impedit molestias
-            quisquam minima velit fuga aliquid culpa nemo officiis vitae
-            pariatur? Sequi, eveniet eum! Assumenda, deleniti!
-          </p> */}
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-1">
-              <AccordionTrigger>Hear {title} Another Night</AccordionTrigger>
-              <AccordionContent>
-                <Link className="hover:underline" to="/events">
-                  {eventDate}
-                </Link>
-              </AccordionContent>
-              <AccordionContent>
-                <Link className="hover:underline" to="/events">
-                  {eventDate}
-                </Link>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
         </div>
-      </div>
+      ) : (
+        <div className="min-h-[40vh] w-full gap-5 p-5">
+          <div className="relative float-left mb-5 mr-5 w-5/12">
+            <img
+              className="size-full rounded-xl object-cover shadow-xl"
+              src={currentEvent.image}
+              alt={currentEvent.alt}
+            />
+            <div className="absolute right-5 top-5 rounded-sm bg-card p-4 font-semibold opacity-85">
+              {new Date(currentEvent.date).toLocaleString(undefined, {
+                month: "short",
+                day: "numeric",
+              })}
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center gap-5">
+              <h2 className="text-2xl font-semibold">{currentEvent.title}</h2>
+              <Link to="#">
+                <CalendarIcon />
+              </Link>
+            </div>
+            <h2 className="text-1xl">{currentEvent.date}</h2>
+            <p>{currentEvent.description}</p>
+          </div>
+          {sortedEvents[currentEvent.title].length > 1 && (
+            <Accordion type="single" collapsible className="w-3/4">
+              <AccordionItem value="item-1">
+                <AccordionTrigger>
+                  Hear {currentEvent.title} Another Night
+                </AccordionTrigger>
+                {sortedEvents[currentEvent.title].map((date, index) => {
+                  return (
+                    <AccordionContent key={index}>
+                      <Link className="hover:underline" to="/events">
+                        {date.toLocaleString(undefined, {
+                          month: "long",
+                          day: "2-digit",
+                        })}
+                      </Link>
+                    </AccordionContent>
+                  );
+                })}
+              </AccordionItem>
+            </Accordion>
+          )}
+        </div>
+      )}
       <div className="flex w-full justify-center">
         <Card>
           <Calendar
             mode="single"
             required
             selected={date}
-            onSelect={setDate}
-            // !MODIFIERS!
-            modifiers={{ electric: electricBananasDates }}
-            // modifiersStyles={{
-            //   electric: {
-            //     color: "white",
-            //   },
-            // }}
-            // modifiersClassNames={{ electric: styles.musicianDate }}
-            // !
+            onSelect={(date) => {
+              setDate(date);
+              setCurrentEvent(
+                mockEvents.find((event) => {
+                  return sameDay(new Date(event.date), new Date(date));
+                }),
+              );
+            }}
+            modifiers={sortedEvents}
             className="relative flex h-full w-full p-5"
             classNames={{
               months: "flex w-full flex-col space-y-4 flex-1",
@@ -122,13 +149,10 @@ export default function events() {
                 buttonVariants({ variant: "ghost" }),
                 "size-24 p-0 font-normal aria-selected:opacity-100 hover:bg-feldgrau hover:text-white",
               ),
-              day: "relative z-20 p-0 text-center text-sm focus-within:relative focus-within:z-20 focus-within:rounded-md [&:has([aria-selected])]:bg-feldgrau [&:has([aria-selected].day-outside)]:bg-feldgrau/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
-              today: cn(
-                buttonVariants({ variant: "default" }),
-                "size-24 p-0 bg-logo-green size-24 hover:bg-logo-green-800",
-              ),
+              day: ":rounded-md relative z-20 p-0 text-center text-sm focus-within:relative focus-within:z-20 focus-within:rounded-md [&:has([aria-selected])]:bg-feldgrau [&:has([aria-selected].day-outside)]:bg-feldgrau/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
+
               selected:
-                "bg-feldgrau text-primary-foreground hover:bg-feldgrau hover:text-primary-foreground focus:bg-feldgrau focus:text-primary-foreground",
+                "rounded-md bg-feldgrau text-primary-foreground hover:bg-feldgrau hover:text-primary-foreground focus:bg-feldgrau focus:text-primary-foreground",
               weekday: "rounded-md w-8 font-semi-bold text-lg",
               weeks: "text-center",
             }}
@@ -141,17 +165,41 @@ export default function events() {
               },
               Day: (props) => {
                 const { children, ...dayProps } = props;
+                let currentModifier = "";
+                Object.keys(sortedEvents).forEach((title) => {
+                  if (dayProps.modifiers[title]) {
+                    currentModifier = title;
+                  }
+                });
+
+                function getImageByTitle(
+                  objectsArray: Event[],
+                  title: string,
+                ): string | undefined {
+                  const object = objectsArray.find(
+                    (obj) => obj.title === title,
+                  );
+                  return object ? object.image : undefined;
+                }
+                const currentPicture = getImageByTitle(
+                  mockEvents,
+                  currentModifier,
+                );
+
                 return (
                   <td {...dayProps}>
                     {children}
-                    {dayProps.modifiers.electric && (
-                      <div className="pointer-events-none absolute top-0 size-full opacity-50">
+                    {currentModifier && (
+                      <div className="pointer-events-none absolute top-0 size-full opacity-40">
                         <img
-                          className="size-full object-cover blur-[2px]"
-                          src={picture}
+                          className="size-full rounded-md object-cover blur-[1px]"
+                          src={currentPicture}
                           alt=""
                         />
                       </div>
+                    )}
+                    {dayProps.modifiers.today && (
+                      <div className="border-gradient-to-r pointer-events-none absolute top-0 box-border size-full rounded-md border-4 border-logo-green"></div>
                     )}
                   </td>
                 );
