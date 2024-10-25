b5fbd12dc97850400afbf9e77a0cd4cccc2817c7
Mike Miller <mikej.miller440@gmail.com>
2024-10-25 00:40:15 -0500

    added more mock events

--- app/mockData/index.ts
+++ app/mockData/index.ts
@@ -1,6 +1,6 @@
 import { type Event } from "~/components/Events";
 import events from "./urban-events.json";
-// import picture from "~/img/sample-musician.jpg";
+import picture from "~/img/sample-musician.jpg";
 import landScapePic from "~/img/musician.jpg";
 import specials from "./urban-specials.json";
 
@@ -12,7 +12,7 @@ nonImageEvents.forEach((currentEvent) => {
   const startTime = new Date(currentEvent.startTime);
   mockEvents.push({
     ...currentEvent,
-    image: landScapePic,
+    image: picture,
     startTime: new Date(currentEvent.startTime).toISOString(),
     endTime: new Date(
       startTime.setTime(startTime.getTime() + 7200000), //add two hours
