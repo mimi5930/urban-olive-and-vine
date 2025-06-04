a8e0bc180f8b57b53e3981950cd94bbc0fbedd81
Mike Miller <mikej.miller440@gmail.com>
2025-06-04 18:55:26 -0500

    added beverages menu and combined wine menus

--- app/documents/index.ts
+++ app/documents/index.ts
@@ -1,17 +1,17 @@
 import mainMenuFile from "./2025 winter Main Menu.pdf";
 import breakfastMenuFile from "./Breakfast Menu - Winter 2025.pdf";
 import beerMenuFile from "./Beer Menu.pdf";
-import redWineMenuFile from "./Red Wine Menu.pdf";
 import summerSpecialsMenuFile from "./Summer 2025 specials.pdf";
-import whiteWineMenuFile from "./White Wine Menu.pdf";
 import yappyHourMenuFile from "./Yappy Hour Menu Summer 2024.pdf";
+import wineMenuFile from "./Wine Menu.pdf";
+import beverageMenuFile from "./BEVERAGE Menu 2024 Summer.pdf";
 
 export {
   mainMenuFile,
   breakfastMenuFile,
   beerMenuFile,
-  redWineMenuFile,
   summerSpecialsMenuFile,
-  whiteWineMenuFile,
   yappyHourMenuFile,
+  wineMenuFile,
+  beverageMenuFile,
 };
